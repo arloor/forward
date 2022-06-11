656ff3103d173a310d0b91a1cf7d784fe645391e
arloor <admin@arloor.com>
2022-06-11 21:52:07 +0800

    网速监控

--- internal/stream/statistics.go
+++ internal/stream/statistics.go
@@ -2,8 +2,6 @@ package stream
 
 import (
 	"encoding/json"
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/opts"
 	"math"
 	"net/http"
 	"strconv"
@@ -250,95 +248,6 @@ func init() {
             </body>
             </html>
 `)
-}
-
-// generate random data for line chart
-func generateLineItems(data []int64) []opts.LineData {
-	items := make([]opts.LineData, 0)
-	for i := 0; i < len(data); i++ {
-		items = append(items, opts.LineData{Value: data[i]})
-	}
-	return items
-}
-
-func LineGraph(w http.ResponseWriter, _ *http.Request) {
-	// create a new line instance
-	line := charts.NewLine()
-
-	// Put data into instance
-	recvRate := recvStatics.GetAll()
-	uploadRate := uploadStatics.GetAll()
-	var max int64 = 0
-	for _, num := range recvRate {
-		if num > max {
-			max = num
-		}
-	}
-	for _, num := range uploadRate {
-		if num > max {
-			max = num
-		}
-	}
-	var interval int64 = 1024 * 1024
-	if max/(interval) > 10 {
-		interval = int64(math.Ceil(float64(max/interval/10))) * interval
-	}
-	splitNum := int(math.Ceil(float64(max) / float64(interval)))
-	max = int64(splitNum) * interval
-	// set some global options like Title/Legend/ToolTip or anything else
-	line.SetGlobalOptions(charts.WithYAxisOpts(opts.YAxis{
-		Max:         max,
-		Type:        "value",
-		Scale:       false,
-		SplitNumber: splitNum,
-		AxisLabel: &opts.AxisLabel{
-			Interval: strconv.FormatInt(interval, 10),
-			Formatter: opts.FuncOpts(`function(value, index) {
-                                if (value == 0) {
-                                    value = '0B';
-                                } else {
-                                    var k = 1024;
-                                    var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-                                    var c = Math.floor(Math.log(value) / Math.log(k));
-                                    value = (value / Math.pow(k, c)) + ' ' + sizes[c];
-                                }
-                                return value;
-                            }`),
-		},
-	}), charts.WithTooltipOpts(opts.Tooltip{
-		Show:      true,
-		Trigger:   "axis",
-		TriggerOn: "",
-		Formatter: opts.FuncOpts(`function(value) {
-							console.log(value);
-                            if (value[0].value == 0) {
-                                value[0].value = '0B';
-                            } else {
-                                var k = 1024;
-                                var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-                                var c = Math.floor(Math.log(value[0].value) / Math.log(k));
-                                value[0].value = (value[0].value / Math.pow(k, c)).toPrecision(4) + ' ' + sizes[c];
-                            }
-                            if (value[1].value == 0) {
-                                value[1].value = '0B';
-                            } else {
-                                var k = 1024;
-                                var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-                                var c = Math.floor(Math.log(value[1].value) / Math.log(k));
-                                value[1].value = (value[1].value / Math.pow(k, c)).toPrecision(4) + ' ' + sizes[c];
-                            }
-                            return value[0].seriesName + ' ' + value[0].value+ '<br/>' + value[1].seriesName + ' ' + value[1].value;
-                        }`),
-		AxisPointer: nil,
-	}),
-		charts.WithInitializationOpts(opts.Initialization{PageTitle: "forward监控", Width: "100%", Height: "100vh"}))
-
-	line.SetXAxis(xAxis).
-		AddSeries("下行", generateLineItems(recvRate)).
-		AddSeries("上行", generateLineItems(uploadRate)).
-		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
-
-	line.Render(w)
 }
 
 func ServeLine(w http.ResponseWriter, r *http.Request) {
